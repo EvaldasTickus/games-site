e6c2fbdfefe0dd9949904f3a195e6513c1b8d9dd
EvaldasTickus <tickusevaldas@gmail.com>
2025-03-25 18:28:30 +0200

    Created Skeletons

---
+++ src/components/CardSkeleton.tsx
@@ -0,0 +1,13 @@
+
+
+const CardSkeleton = () => {
+  return (
+    <div className="bg-gray-900 rounded-xl">
+      <div className="h-[200px] bg-gray-800 rounded-xl"></div>
+      <div className="h-7 bg-gray-800 mt-5 rounded-xl"></div>
+      <div className="h-5 bg-gray-800 mt-5 rounded-xl"></div>
+    </div>
+  )
+}
+
+export default CardSkeleton
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,16 @@
 import useGames from "../hooks/useGames";
+import CardSkeleton from "./CardSkeleton";
 import GameCard from "./GameCard";
 
-
 const GameGrid = () => {
-  const {games, error} = useGames()
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
     <>
       {error && <p>{error}</p>}
       <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2.5 p-2.5">
+        {isLoading && skeletons.map(skeleton => <CardSkeleton key={skeleton}></CardSkeleton>)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
